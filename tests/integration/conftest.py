62872912475fcaa57cb38f0ad9bcfba31e597126
Admin <janithashri@gmail.com>
2025-08-06 14:29:20 +0530

    fix(route53): Prevent circular SUBZONE relationships
    Signed-off-by: Admin <janithashri@gmail.com>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,19 +1,17 @@
+import logging
+
 import neo4j
 import pytest
-from dynaconf import Dynaconf
 
-# This explicitly loads your config.yaml file for the test run.
-settings = Dynaconf(
-    settings_files=['config.yaml'],
-)
+from tests.integration import settings
 
-@pytest.fixture(scope="function")
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("neo4j").setLevel(logging.WARNING)
+
+
+@pytest.fixture(scope="module")
 def neo4j_session():
-    # This `auth` tuple is required to connect to a password-protected database
-    auth = (settings.get("NEO4J_USER"), settings.get("NEO4J_SECRET"))
-    driver = neo4j.GraphDatabase.driver(settings.get("NEO4J_URI"), auth=auth)
-
+    driver = neo4j.GraphDatabase.driver(settings.get("NEO4J_URL"))
     with driver.session() as session:
         yield session
-        # This runs after each test to ensure the database is clean for the next test
         session.run("MATCH (n) DETACH DELETE n;")
